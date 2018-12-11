170ae15694d8d1661f5f82267048e602638f33fa
Conzenite17 <Conzenite17@gmail.com>
2018-12-11 16:17:06 +0000

    added lines to show grid nice

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample3.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Tic Tac Toe the Extreme");
+        primaryStage.setScene(new Scene(root, 400, 700));
         primaryStage.show();
     }
 
